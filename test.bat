4d4ec6a1af6506f50d91c156ae096895b09d368f
Rann Day <5001592+rannday@users.noreply.github.com>
2025-07-06 15:46:21 -0400

    added kea-docker git pull. new integrations test.

--- test.bat
+++ test.bat
@@ -5,12 +5,15 @@ setlocal enabledelayedexpansion
 set CONTAINER_NAME=kea-int-test
 set IMAGE_NAME=kea-custom:latest
 set KEA_DOCKER_PATH=..\kea-docker
-set UPDATE_DOCKER_REPO=false
 
-:: Check for -i flag
+:: Flags
 set RUN_INTEGRATION=false
+set FORCE_REBUILD=false
+
+:: Parse CLI args
 for %%x in (%*) do (
     if "%%x"=="-i" set RUN_INTEGRATION=true
+    if "%%x"=="-r" set FORCE_REBUILD=true
 )
 
 echo.
@@ -24,7 +27,7 @@ if "%RUN_INTEGRATION%"=="true" (
     echo.
     echo === Preparing integration test environment ===
 
-    :: Check if kea-docker repo is cloned
+    :: Ensure kea-docker repo exists
     if not exist "%KEA_DOCKER_PATH%\Dockerfile" (
         echo kea-docker not found. Cloning...
         git clone https://github.com/rannday/kea-docker.git %KEA_DOCKER_PATH%
@@ -33,34 +36,44 @@ if "%RUN_INTEGRATION%"=="true" (
             exit /b 1
         )
     ) else (
-        echo kea-docker repo found.
-        if "%UPDATE_DOCKER_REPO%"=="true" (
-            echo Updating kea-docker...
-            pushd %KEA_DOCKER_PATH%
-            git pull
+        echo kea-docker repo found. Pulling latest changes...
+        pushd %KEA_DOCKER_PATH%
+        git pull
+        if errorlevel 1 (
+            echo git pull failed. Aborting.
             popd
+            exit /b 1
         )
+        popd
     )
 
-    echo Checking for Docker image "%IMAGE_NAME%"...
+    echo.
+    echo === Checking Docker image: %IMAGE_NAME% ===
     for /f %%i in ('docker images -q %IMAGE_NAME%') do set IMAGE_EXISTS=%%i
 
-    if not defined IMAGE_EXISTS (
-        echo Building Docker image from %KEA_DOCKER_PATH%...
+    if "%FORCE_REBUILD%"=="true" (
+        echo Forcing rebuild of Docker image...
+        docker build --no-cache -t %IMAGE_NAME% %KEA_DOCKER_PATH%
+        if errorlevel 1 (
+            echo Docker build failed. Aborting.
+            exit /b 1
+        )
+    ) else if not defined IMAGE_EXISTS (
+        echo Building Docker image...
         docker build -t %IMAGE_NAME% %KEA_DOCKER_PATH%
         if errorlevel 1 (
             echo Docker build failed. Aborting.
             exit /b 1
         )
     ) else (
-        echo Image already built.
+        echo Image already built. Skipping rebuild.
     )
 
-    echo Checking if container "%CONTAINER_NAME%" is running...
+    echo.
+    echo === Starting Kea container ===
     for /f %%i in ('docker ps -q --filter "name=%CONTAINER_NAME%"') do set CONTAINER_RUNNING=%%i
 
     if not defined CONTAINER_RUNNING (
-        echo Starting Kea container...
         docker run -d --rm --name %CONTAINER_NAME% -p 8000:8000 %IMAGE_NAME%
         set CLEANUP_CONTAINER=true
         echo Waiting for Kea to be ready...
